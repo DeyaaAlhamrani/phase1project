1f6af4956de9eaf727209c4c556efd4a3a3d6c5f
deyaa h <deyaa1418@gmail.com>
2022-09-08 06:22:11 +0300

    JS done

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-var fs = require('fs');
-var htmlcontent = "<html></html>";
-fs.writeFile("/blog4.html", htmlcontent)
+var x = document.querySelector(".readMore");
+
+function readML(el){
+    console.log(el.className);
+    console.log(el.style.display);
+        if(el.className =="readMore"){
+            el.style.display ="none";
+            el.nextElementSibling.style.display="inline-block";
+        }
+        else{
+            el.style.display ="none";
+            el.previousElementSibling.style.display="inline-block";
+        }
+        
+    };
+
+
+
